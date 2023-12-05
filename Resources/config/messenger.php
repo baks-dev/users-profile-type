b8d23d8801471ace4d3958a4d52242743f99bbb5
kingstar <admin@baks.dev>
2023-12-05 17:39:19 +0300

    Baks Development

--- Resources/config/messenger.php
+++ Resources/config/messenger.php
@@ -29,7 +29,7 @@ return static function (FrameworkConfig $framework) {
         ->dsn('%env(MESSENGER_TRANSPORT_DSN)%')
         ->options(['queue_name' => 'users-profile-type'])
         ->retryStrategy()
-        ->maxRetries(5)
+        ->maxRetries(3)
         ->delay(1000)
         ->maxDelay(0)
         ->multiplier(3) // увеличиваем задержку перед каждой повторной попыткой
