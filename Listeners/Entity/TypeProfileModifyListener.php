400f9210ced4784c8a8ab57c02d4df6f2e09bb48
kingstar <admin@baks.dev>
2024-03-29 18:48:52 +0300

    Baks Development

--- Listeners/Entity/TypeProfileModifyListener.php
+++ Listeners/Entity/TypeProfileModifyListener.php
@@ -25,11 +25,13 @@ namespace BaksDev\Users\Profile\TypeProfile\Listeners\Entity;
 
 use BaksDev\Core\Type\Ip\IpAddress;
 use BaksDev\Users\Profile\TypeProfile\Entity\Modify\TypeProfileModify;
+use BaksDev\Users\User\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
 use Doctrine\ORM\Events;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\SwitchUserToken;
 
 #[AsEntityListener(event: Events::prePersist, method: 'prePersist', entity: TypeProfileModify::class)]
 final class TypeProfileModifyListener
@@ -50,9 +52,16 @@ final class TypeProfileModifyListener
     {
         $token = $this->token->getToken();
 
-        if ($token)
-        {
+        if ($token) {
+
             $data->setUsr($token->getUser());
+
+            if($token instanceof SwitchUserToken)
+            {
+                /** @var User $originalUser */
+                $originalUser = $token->getOriginalToken()->getUser();
+                $data->setUsr($originalUser);
+            }
         }
 
         /* Если пользователь не из консоли */
