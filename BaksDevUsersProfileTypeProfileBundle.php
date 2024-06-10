5550d3acfc6244b7b8577b0f8a015542f06534a3
kingstar <admin@baks.dev>
2024-06-10 10:44:16 +0300

    Baks Development

--- BaksDevUsersProfileTypeProfileBundle.php
+++ BaksDevUsersProfileTypeProfileBundle.php
@@ -12,6 +12,9 @@ declare(strict_types=1);
 
 namespace BaksDev\Users\Profile\TypeProfile;
 
+use BaksDev\Users\Profile\TypeProfile\Type\Id\Choice\Collection\TypeProfileInterface;
+use BaksDev\Users\Profile\TypeProfile\Type\Id\Choice\TypeProfileOrganization;
+use BaksDev\Users\Profile\TypeProfile\Type\Id\Choice\TypeProfileUser;
 use DirectoryIterator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -23,5 +26,31 @@ class BaksDevUsersProfileTypeProfileBundle extends AbstractBundle
     public const NAMESPACE = __NAMESPACE__.'\\';
 
     public const PATH = __DIR__.DIRECTORY_SEPARATOR;
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $services = $container->services()
+            ->defaults()
+            ->autowire()
+            ->autoconfigure();
+
+        $services->load(self::NAMESPACE, self::PATH)
+            ->exclude([
+                self::PATH.'{Entity,Resources,Type}',
+                self::PATH.'**/*Message.php',
+                self::PATH.'**/*DTO.php',
+            ]);
+
+        /* Типы профилей */
+        $services->load(
+            self::NAMESPACE.'Type\Id\Choice\\',
+            self::PATH.'Type/Id/Choice'
+        );
+
+        /** @see https://symfony.com/doc/current/service_container/autowiring.html#dealing-with-multiple-implementations-of-the-same-type */
+
+        $services->alias(TypeProfileInterface::class.' $typeProfileUser', TypeProfileUser::class);
+        $services->alias(TypeProfileInterface::class.' $typeProfileOrganization', TypeProfileOrganization::class);
+    }
 	
 }
