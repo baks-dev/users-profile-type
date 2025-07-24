901c1045615a47111531dace0bc822db149806fd
MichelAngelo <baksdevelopmen@gmail.com>
2025-07-24 18:30:48 +0300

    BaksDevelopment
    Signed-off-by: MichelAngelo <baksdevelopmen@gmail.com>

--- Type/Id/Tests/TypeProfileUidTest.php
+++ Type/Id/Tests/TypeProfileUidTest.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -55,7 +55,9 @@ final class TypeProfileUidTest extends KernelTestCase
             self::assertTrue($TypeProfileUid->equals($TypeProfileUid)); // объект класса
 
             $TypeProfileType = new TypeProfileType();
-            $platform = $this->getMockForAbstractClass(AbstractPlatform::class);
+            $platform = $this
+                ->getMockBuilder(AbstractPlatform::class)
+                ->getMock();
 
             $convertToDatabase = $TypeProfileType->convertToDatabaseValue($TypeProfileUid, $platform);
             self::assertEquals($TypeProfileUid->getTypeProfileValue(), $convertToDatabase);
