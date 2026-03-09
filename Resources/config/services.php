4b6a3c2f3f82240cdc10a71b4f86a87633f3ee9c
MichelAngelo <baksdevelopmen@gmail.com>
2026-03-10 01:30:36 +0300

    BaksDevelopment
    Signed-off-by: MichelAngelo <baksdevelopmen@gmail.com>

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -33,8 +33,7 @@ return static function(ContainerConfigurator $configurator) {
     $services = $configurator->services()
         ->defaults()
         ->autowire()
-        ->autoconfigure()
-        ->public();
+        ->autoconfigure(); //->public();
 
     $NAMESPACE = BaksDevUsersProfileTypeProfileBundle::NAMESPACE;
     $PATH = BaksDevUsersProfileTypeProfileBundle::PATH;
